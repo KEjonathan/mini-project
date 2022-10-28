7c6d45b2424c7321b36f9740190acf1178f88a8b
Kejonathan <kejonathan75@gmail.com>
2022-10-28 12:57:56 +0300

    Emma
    Final Edits

---
+++ app/Models/Form.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Form extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'q1',
+        'q2',
+        'q3',
+        'q4',
+        'q5',
+        'q6',
+        'q7',
+        'unit_id'
+    ];
+
+    public function form(){
+        $this->belongsTo(Unit::class);
+    }
+}
